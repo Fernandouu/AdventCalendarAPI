3966c4a9ef1dcc0dd2974c0922f3716a0779846d
Fernandouu <20190369@ricaldone.edu.sv>
2024-12-03 21:36:26 -0600

    console.log

--- src/config.js
+++ src/config.js
@@ -8,3 +8,12 @@ export const DB_PASSWORD = process.env.DB_PASSWORD
 export const DB_HOST = process.env.DB_HOST  
 export const DB_DATABASE = process.env.DB_DATABASE
 export const DB_PORT = process.env.DB_PORT || 3306;
+
+console.log({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database: DB_DATABASE,
+  });
+  
