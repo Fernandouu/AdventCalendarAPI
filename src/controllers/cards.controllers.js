985e63bb5176083f95b1224b57ee1bbb6171c05a
Fernandouu <20190369@ricaldone.edu.sv>
2024-12-03 22:26:18 -0600

    ca

--- src/controllers/cards.controllers.js
+++ src/controllers/cards.controllers.js
@@ -1,25 +1,25 @@
 import {pool} from '../db.js';
 
 export const getCards = async (req, res) => {
-    const [rows] =  await pool.query('SELECT * FROM cardsCalendario')
+    const [rows] =  await pool.query('SELECT * FROM CardsCalendario')
     res.json(rows)
 };
 
 export const getOneCards = async (req, res) => {
-    const [rows] =  await pool.query('SELECT * FROM cardsCalendario WHERE id_card = ?', [req.params.id] )
+    const [rows] =  await pool.query('SELECT * FROM CardsCalendario WHERE id_card = ?', [req.params.id] )
     if(rows.length <= 0) return res.status(404).json({message: 'Sorpresa no disponible'})
         res.json(rows[0])    
 };
 
 export const UpdateStatusCard = async (req, res) => {
     const {id} = req.params
-    const[result] =  await pool.query('UPDATE cardsCalendario SET estado_card = "Abierta" WHERE id_card = ?', [ id])
+    const[result] =  await pool.query('UPDATE CardsCalendario SET estado_card = "Abierta" WHERE id_card = ?', [ id])
     if(result.affectedRows > 0){
         res.json({message: `Sorpresa ${id} actualizada`})
     }else {
         res.status(404).json({message: 'Sorpresa no disponible'})
     }
 
-    const [rows] =  await pool.query('SELECT * FROM cardsCalendario WHERE id_card = ?', [id] )
+    const [rows] =  await pool.query('SELECT * FROM CardsCalendario WHERE id_card = ?', [id] )
     console.log(rows[0])
 }
